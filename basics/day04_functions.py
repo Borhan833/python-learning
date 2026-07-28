48dab4cae66c124b43b6ae701590c3a490ac968f
Borhan833 <Borhan8378@gmail.com>
2026-07-28 16:56:35 +0330

    Create day04_functions.py

---
+++ basics/day04_functions.py
@@ -0,0 +1,12 @@
+def hello():
+    print("Hello")
+hello()
+def welcome():
+    print("Welcome To Python.")
+welcome()
+welcome()
+def goodbye():
+    print("Good Bye.")
+goodbye()
+goodbye()
+goodbye()
